2d939e7c598bae19123519f071952e6fe77266fe
Roger! <icarus.gk@gmail.com>
2022-12-03 17:36:44 -0600

    add: auth tests

--- api/api/tests.py
+++ api/api/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+class TestUserAuth(TestCase):
+  def setUp(self) -> None:
+    self.c = Client()
+    self.user = {
+      'username': 'test',
+      'password': 'test_user'
+    }
+    User.objects.create_user(**self.user)
+    
+
+  def test_user_login(self):
+    response = self.c.post('/api/auth/login', self.user)
+    # Assert response
+    self.assertContains(response, status_code=200, text="{}")
+
+    # Test access token and session id
+    self.assertTrue(response.cookies.get('access_token'))
+    self.assertTrue(response.cookies.get('sessionid'))
+
+
+  
+  def test_user_logout(self):
+    self.test_user_login()
+    expected_message = { 'message': 'You are logged out' }
+    
+    response = self.c.post('/api/auth/logout')
+    self.assertEqual(response.status_code, 200)
+    self.assertEqual(response.json(), expected_message)
+    
+  
