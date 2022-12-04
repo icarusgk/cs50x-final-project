9b09a38eff60ddd215e00fdd087b739a0cdc314b
Roger! <icarus.gk@gmail.com>
2022-12-03 23:25:10 -0600

    add: server config and dependencies

--- kanban-nuxt/nuxt.config.ts
+++ kanban-nuxt/nuxt.config.ts
@@ -1,4 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  
+  modules: [
+    '@nuxtjs/tailwindcss',
+    'nuxt-icon',
+    ['@pinia/nuxt', {
+      autoImports: ['defineStore', 'acceptHMRUpdate']
+    }],
+    '@vueuse/nuxt',
+    "@formkit/nuxt"
+  ],
+  imports: {
+    dirs: ['stores']
+  },
+  runtimeConfig: {
+    public: {
+      API_BASE: 'http://127.0.0.1:8000/api/'
+    }
+  }
 })
